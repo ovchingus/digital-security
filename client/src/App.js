469b5fd13e728c335b5c8fac4b04d684da6f87ab
Vladimir Ovechkin <ovchingus@gmail.com>
2020-01-20 18:55:01 +0300

    Add tabs

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+
+const TabPanel = ({ children, value, index }) =>
+  value === index && <div>{children}</div>
 
 const App = () => {
+  const [currentTab, setCurrentTab] = useState(0)
+
+  const handleChange = (event, newValue) => {
+    setCurrentTab(newValue)
+  }
+
   return (
-    <div>
-      hi
-    </div>
+    <>
+      <AppBar position='static'>
+        <Tabs
+          value={currentTab}
+          onChange={handleChange}
+          centered
+        >
+          <Tab label='Книги' />
+          <Tab label='Авторы' />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={currentTab} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={currentTab} index={1}>
+        Item Two
+      </TabPanel>
+    </>
   )
 }
 
